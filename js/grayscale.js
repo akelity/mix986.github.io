063aaf26de30eeba7c018152b8b5626799d50f84
Michel Murabito <michelmurabito@michels-MacBook-Air.local>
2017-03-14 23:30:34 +0100

    Edizione Milano 2017

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,17 +38,17 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 14,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(41.9008553, 12.5010826), // Luiss Enlabs Calabiana
+        center: new google.maps.LatLng(45.464211, 9.191383), // Milano
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -160,7 +160,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(41.9008553, 12.5010826);
+    var myLatLng = new google.maps.LatLng(45.464211, 9.191383);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
